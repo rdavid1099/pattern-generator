4ac15574228f12eb0b865b0eee9d629a6787de05
Ryan Workman <rdavid1099@gmail.com>
2018-05-01 09:53:07 -0600

    Change name and add skips

--- alphanumeric_pattern_generator.rb
+++ alphanumeric_pattern_generator.rb
@@ -2,7 +2,7 @@ module BookKeeping
   VERSION = 1
 end
 
-class PatternGenerator
+class AlphanumericPatternGenerator
   def initialize(pattern)
     @pattern = pattern
     @pattern_elements = get_pattern_elements
